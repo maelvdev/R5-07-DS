8b590ad36cb5c5fa476281c3b5842fef0a393564
Mael VAUTHIER <maelv.contact@gmail.com>
2025-10-08 14:15:47 +0000

    Création de Q3.java

---
+++ Q3.java
@@ -0,0 +1,16 @@
+public class Q3
+{
+    public static void suite(int n)
+    {
+        for (int i=1; i<=n; i++)
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args)
+    {
+        suite(5);
+    }
+}
