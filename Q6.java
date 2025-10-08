4c12352acc5ff92b196da9ffbd7bce3c6b3d2bd2
Mael VAUTHIER <maelv.contact@gmail.com>
2025-10-08 16:47:19 +0200

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,19 @@
+public class Q6
+{
+    public static void triangle(int n)
+    {
+        for (int i=1; i<=n; i++)
+        {
+            for (int j=1 ;j<=i; j++)
+            {
+                System.out.print(x);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        triangle(4);
+    }
+}
